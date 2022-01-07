95a1407e6764d12b52b15891209b60b4153502d7
Fernando <fernando.luiz92@hotmail.com>
2022-01-06 21:51:28 -0300

    Implementado Helper

---
+++ src/livraria/helper/Carrinho.java
@@ -0,0 +1,47 @@
+package livraria.helper;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import livraria.entity.Livro;
+
+public class Carrinho {
+	
+	// As duas colecoes sao gerenciadas simultaneamente, Uma tem objetos view, e a outra objetos entity.
+	// Ambas sao Set, para evitar que livros iguais possam ser adicionados mais de uma vez no carrinho.
+	private Set<Livro> livros = new LinkedHashSet<Livro>();
+	
+	/**
+	 * Adiciona um livro ao carrinho
+	 * @param livro
+	 */
+	public void adicionarItem(Livro livro) {
+		livros.add(livro);
+	}
+	/**
+	 * Remove um livro do carrinho
+	 * @param livroId
+	 */
+	public void removerItem(Integer livroId) {
+		Livro livro = new Livro();
+		livro.setId(livroId);
+		livros.remove(livro);
+	}
+	
+	/**
+	 * Obtem o valor total do carrinho, somando os valores dos itens
+	 * @return
+	 */
+	public Double getValorTotal() {
+		double total = 0.0;
+		for(Livro livro : livros) {
+			total += livro.getPreco();
+		}
+		return total;
+	}
+	
+	public Set<Livro> getItens(){return livros;}
+	
+	public Set<Livro> getLivros(){return livros;}
+
+}
